a5022f78e7870d610375194d16d26cf643a64330
erikturkovic <93389338+erikturkovic@users.noreply.github.com>
2024-09-08 16:39:47 +0200

    Dodan time frequency toolbox i generiran signal linearnom modulacijom frekvencije
    Time frequency toolbox dodan u path. Pocetak stvaranja signala po tom toolboxu. Prvi generiran signal je jednostavni fmlin.

--- MatlabKod.m
+++ MatlabKod.m
@@ -1,49 +1,12 @@
-%% 
+% linear frequency modulation
+sig = fmlin(128, 0, 0.5);
+% linear frequency modulation
+sig = fmlin(128, 0, 0.5);
 
-fs = 1000; % Frequency of sampling (Hz)
-t = 0:1/fs:10-1/fs; % Time vector
-whiteNoise = randn(size(t)); % White noise signal
-
-% Plot the white noise signal in the time domain
 figure;
-plot(t, whiteNoise);
-xlabel('Time (s)');
+plot(real(sig));
+xlabel('Time');
 ylabel('Amplitude');
-title('White Noise in Time Domain');
+title('Time-Domain Representation of linear frequency modulation ');
 grid on;
 
-
-
-%% 
-N = length(whiteNoise); % Length of the signal
-f = (0:N-1)*(fs/N); % Frequency vector
-
-% Compute the FFT of the signal
-Y = fft(whiteNoise);
-
-% Plot the magnitude of the FFT (single-sided spectrum)
-figure;
-plot(f(1:floor(N/2)), abs(Y(1:floor(N/2))));
-xlabel('Frequency (Hz)');
-ylabel('Magnitude');
-title('White Noise in Frequency Domain (FFT)');
-grid on;
-
-%% 
-% Using the pwelch method for PSD
-[pxx, f] = pwelch(whiteNoise, [], [], [], fs);
-
-% Plot the PSD
-figure;
-plot(f, 10*log10(pxx)); % Convert to dB scale
-xlabel('Frequency (Hz)');
-ylabel('Power/Frequency (dB/Hz)');
-title('Power Spectral Density (PSD)');
-grid on;
-
-%% 
-
-% Plot spectrogram
-figure;
-spectrogram(whiteNoise,256,250,256,fs,'yaxis');
-title('Spectrogram of White Noise');
