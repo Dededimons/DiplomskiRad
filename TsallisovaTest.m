03ae8d044e5afc33f9cb253d795787c770ada36b
erikturkovic <60065884+Dededimons@users.noreply.github.com>
2025-09-17 15:06:36 +0200

    Lin, sin, par modul za renyi

---
+++ TsallisovaTest.m
@@ -0,0 +1,6 @@
+clear; clc; close all;
+
+N = 128;
+pocetnaFrekvencija = 0; krajnjaFrekvencija = 0.5; 
+brojSimulacija = 100;  
+SNR = [10, 5, 1]; 
--- oldcode/PrediplomskiReferenca.m
+++ oldcode/PrediplomskiReferenca.m
@@ -1,6 +1,6 @@
 SNR=0
-N=128;
-sig1=fmlin(N,0.1,0.250);
+N=1000;
+sig1=fmpar(N);
 plot(real(sig1))
 title('sig1 Linear frequency modulation');
 axis([1 N -1 1]);
