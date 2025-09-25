ee5190ffc9812646576468c7d62a68eb910a8903
erikturkovic <60065884+Dededimons@users.noreply.github.com>
2025-09-25 15:40:42 +0200

    Renyi entropy  pink,brown,blue,purple noise

--- RenyiEntropy.m
+++ RenyiEntropy.m
@@ -6,58 +6,66 @@ brojSimulacija = 100;
 alphaRenyi = 3;  
 SNR = [10, 5, 1]; 
 
-rezultati_fmlin = zeros(length(SNR), 1);
-rezultati_fmsin = zeros(length(SNR), 1);
-rezultati_fmpar = zeros(length(SNR), 1);
+pinkGen   = dsp.ColoredNoise('Color','pink','SamplesPerFrame',N,'NumChannels',1);
+brownGen  = dsp.ColoredNoise('Color','brown','SamplesPerFrame',N,'NumChannels',1);
+blueGen   = dsp.ColoredNoise('Color','blue','SamplesPerFrame',N,'NumChannels',1);
+purpleGen = dsp.ColoredNoise('Color','purple','SamplesPerFrame',N,'NumChannels',1);
 
-for indeks = 1:length(SNR)
-    razinaSNR = SNR(indeks);
-    vrijednostiRenyi = zeros(brojSimulacija, 1); 
-    for simulacija = 1:brojSimulacija
-        signallin = fmlin(N, pocetnaFrekvencija, krajnjaFrekvencija);    
-        noise = noisecg(N);
-        noisySignal = sigmerge(signallin, noise, razinaSNR);
-        [PSD, ~] = pwelch(noisySignal, hamming(256), 128, 1024, 1);
-        PSD = PSD / sum(PSD);
-        renyiVal = (1/(1 - alphaRenyi)) * log(sum(PSD.^alphaRenyi));
-        vrijednostiRenyi(simulacija) = renyiVal;
+noiseTypes = {'white','pink','brown','blue','purple'};
+signalTypes = {'fmlin','fmsin','fmpar'};
+
+results = zeros(length(SNR), length(signalTypes)*length(noiseTypes));
+
+for sType = 1:length(signalTypes)
+    for nType = 1:length(noiseTypes)
+        for indeks = 1:length(SNR)
+            razinaSNR = SNR(indeks);
+            vrijednostiRenyi = zeros(brojSimulacija,1);
+            
+            for simulacija = 1:brojSimulacija
+                switch signalTypes{sType}
+                    case 'fmlin'
+                        signal = fmlin(N, pocetnaFrekvencija, krajnjaFrekvencija);
+                    case 'fmsin'
+                        signal = fmsin(N);
+                    case 'fmpar'
+                        p1 = [1, 0];  
+                        p2 = [N/2, 0.25]; 
+                        p3 = [N, 0.4]; 
+                        [signal,~] = fmpar(N, p1, p2, p3);
+                end
+                
+                switch noiseTypes{nType}
+                    case 'white'
+                        noise = noisecg(N);
+                    case 'pink'
+                        noise = pinkGen();
+                    case 'brown'
+                        noise = brownGen();
+                    case 'blue'
+                        noise = blueGen();
+                    case 'purple'
+                        noise = purpleGen();
+                end
+                
+                noisySignal = sigmerge(signal, noise, razinaSNR);
+                [PSD,~] = pwelch(noisySignal, hamming(256), 128, 1024, 1);
+                PSD = PSD / sum(PSD);
+                renyiVal = (1/(1-alphaRenyi)) * log(sum(PSD.^alphaRenyi));
+                vrijednostiRenyi(simulacija) = renyiVal;
+            end
+            colIdx = (sType-1)*length(noiseTypes) + nType;
+            results(indeks,colIdx) = mean(vrijednostiRenyi);
+        end
     end
-    rezultati_fmlin(indeks) = mean(vrijednostiRenyi);
 end
 
-for indeks = 1:length(SNR)
-    razinaSNR = SNR(indeks);
-    vrijednostiRenyi = zeros(brojSimulacija, 1); 
-    for simulacija = 1:brojSimulacija
-        signalsin = fmsin(N);    
-        noise = noisecg(N);
-        noisySignal = sigmerge(signalsin, noise, razinaSNR);
-        [PSD, ~] = pwelch(noisySignal, hamming(256), 128, 1024, 1);
-        PSD = PSD / sum(PSD);
-        renyiVal = (1/(1 - alphaRenyi)) * log(sum(PSD.^alphaRenyi));
-        vrijednostiRenyi(simulacija) = renyiVal;
+varNames = {};
+for sType = 1:length(signalTypes)
+    for nType = 1:length(noiseTypes)
+        varNames{end+1} = [signalTypes{sType} '_' noiseTypes{nType}];
     end
-    rezultati_fmsin(indeks) = mean(vrijednostiRenyi);
 end
 
-for indeks = 1:length(SNR)
-    razinaSNR = SNR(indeks);
-    vrijednostiRenyi = zeros(brojSimulacija, 1); 
-    for simulacija = 1:brojSimulacija
-        p1 = [1, 0];  
-        p2 = [N/2, 0.25]; 
-        p3 = [N, 0.4]; 
-        [signalpar, ~] = fmpar(N, p1, p2, p3);
-        noise = noisecg(N);
-        noisySignal = sigmerge(signalpar, noise, razinaSNR);
-        [PSD, ~] = pwelch(noisySignal, hamming(256), 128, 1024, 1);
-        PSD = PSD / sum(PSD);
-        renyiVal = (1/(1 - alphaRenyi)) * log(sum(PSD.^alphaRenyi));
-        vrijednostiRenyi(simulacija) = renyiVal;
-    end
-    rezultati_fmpar(indeks) = mean(vrijednostiRenyi);
-end
-
-T = table(SNR.', rezultati_fmlin, rezultati_fmsin, rezultati_fmpar, ...
-    'VariableNames', {'SNR','Renyi_fmlin','Renyi_fmsin','Renyi_fmpar'});
+T = array2table(results, 'VariableNames', varNames, 'RowNames', strcat("SNR_", string(SNR)));
 disp(T);
