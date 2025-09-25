6b765197b119dd85833a4307dfa740cef09db08d
erikturkovic <60065884+Dededimons@users.noreply.github.com>
2025-09-25 16:55:24 +0200

    KolmogorovEntropy begining

---
+++ KolmogorovEntropy.m
@@ -0,0 +1,73 @@
+clear; clc; close all;
+
+N = 1024;
+startFrequency = 0; endFrequency = 0.5; 
+numSimulations = 100;  
+SNR = [10, 5, 1]; 
+
+pinkGen   = dsp.ColoredNoise('Color','pink','SamplesPerFrame',N,'NumChannels',1);
+brownGen  = dsp.ColoredNoise('Color','brown','SamplesPerFrame',N,'NumChannels',1);
+blueGen   = dsp.ColoredNoise('Color','blue','SamplesPerFrame',N,'NumChannels',1);
+purpleGen = dsp.ColoredNoise('Color','purple','SamplesPerFrame',N,'NumChannels',1);
+
+noiseTypes = {'white','pink','brown','blue','purple'};
+signalTypes = {'fmlin','fmsin','fmpar'};
+
+results = zeros(length(SNR), length(signalTypes)*length(noiseTypes));
+
+for sType = 1:length(signalTypes)
+    for nType = 1:length(noiseTypes)
+        for idx = 1:length(SNR)
+            currentSNR = SNR(idx);
+            entropyValues = zeros(numSimulations,1);
+            
+            for sim = 1:numSimulations
+                switch signalTypes{sType}
+                    case 'fmlin'
+                        signal = fmlin(N, startFrequency, endFrequency);
+                    case 'fmsin'
+                        signal = fmsin(N);
+                    case 'fmpar'
+                        p1 = [1, 0];  
+                        p2 = [N/2, 0.25]; 
+                        p3 = [N, 0.4]; 
+                        [signal,~] = fmpar(N, p1, p2, p3);
+                end
+                
+                switch noiseTypes{nType}
+                    case 'white'
+                        noise = noisecg(N);
+                    case 'pink'
+                        noise = pinkGen();
+                    case 'brown'
+                        noise = brownGen();
+                    case 'blue'
+                        noise = blueGen();
+                    case 'purple'
+                        noise = purpleGen();
+                end
+                
+                noisySignal = sigmerge(signal, noise, currentSNR);
+                [PSD,~] = pwelch(noisySignal, hamming(256), 128, 1024, 1);
+                PSD = PSD / sum(PSD);
+                
+                p = PSD(PSD > 0);
+                kolmogorovVal = -sum(p .* log(p));
+                
+                entropyValues(sim) = kolmogorovVal;
+            end
+            colIdx = (sType-1)*length(noiseTypes) + nType;
+            results(idx,colIdx) = mean(entropyValues);
+        end
+    end
+end
+
+varNames = {};
+for sType = 1:length(signalTypes)
+    for nType = 1:length(noiseTypes)
+        varNames{end+1} = [signalTypes{sType} '_' noiseTypes{nType}];
+    end
+end
+
+T = array2table(results, 'VariableNames', varNames, 'RowNames', strcat("SNR_", string(SNR)));
+disp(T);
